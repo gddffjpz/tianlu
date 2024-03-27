f0f1c337d906100c101fc5cac93cf11125e4b92a
荆培钊 <1318836484@.qqcom>
2024-03-27 15:38:16 +0800

    实验

--- src/main/java/com/itheima/controller/SecurityConfig.java
+++ src/main/java/com/itheima/controller/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+//sss
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
