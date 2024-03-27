4459123c643a54ecf9fec56ef0cb179100d3238b
荆培钊 <1318836484@.qqcom>
2024-03-27 15:33:16 +0800

    Merge branch 'master' of https://github.com/gddffjpz/tianlu
    # Conflicts:
    #	tianlu.iml
    #	tlias.sql

--- src/main/java/com/itheima/controller/HelloController.java
+++ src/main/java/com/itheima/controller/HelloController.java
@@ -1,2 +1,14 @@
-package com.itheima.controller;public class HelloController {
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, welcome to the secured API!";
+    }
 }
+
